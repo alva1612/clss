403170b8326a8dddf133bd5a7eb41e05869c4393
Alvaro <alvaro.guillenc1612@gmail.com>
2023-06-24 16:24:57 -0500

    prettier

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 function toVal(mix) {
-    if (!mix) return false
-    let str = ""
-    str += mix
-    return str
-  }
-  
+  if (!mix) return false;
+  let str = "";
+  str += mix;
+  return str;
+}
+
 export function clss(...args) {
-    let i = 0,
-      tmp,
-      x,
-      str = ""
-    while (i < args.length) {
-      if ((tmp = args[i++])) {
-        if ((x = toVal(tmp))) {
-          str && (str += " ")
-          str += x
-        }
+  let i = 0,
+    tmp,
+    x,
+    str = "";
+  while (i < args.length) {
+    if ((tmp = args[i++])) {
+      if ((x = toVal(tmp))) {
+        str && (str += " ");
+        str += x;
       }
     }
-    return str
   }
+  return str;
+}
